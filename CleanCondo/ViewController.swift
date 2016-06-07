e5fc1963ec453d27bd60b16ca2809ae4143e0ea5
neon0619 <neon0619@gmail.com>
2016-06-08 05:39:46 +0800

    First Commit

---
+++ CleanCondo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CleanCondo
+//
+//  Created by --=_t0tep_=-- on 08/06/2016.
+//  Copyright © 2016 --=_t0tep_=--. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
